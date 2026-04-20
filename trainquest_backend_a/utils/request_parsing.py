d1530d9059e271034372cd6d32a799331b0a7815
yeli041029-hash <ye.li041029@gmail.com>
2026-04-20 13:55:33 +0800

    Delete trainquest_backend_a directory

--- trainquest_backend_a/utils/request_parsing.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify, request
-
-
-def get_json_object():
-    data = request.get_json(silent=True)
-    if not isinstance(data, dict):
-        return None, (jsonify({"message": "Request body must be a JSON object"}), 400)
-
-    return data, None
